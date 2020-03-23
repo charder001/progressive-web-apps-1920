ec0f127bcbfdbd860618de4a81ae51e6dc66c89f
Charder001 <rick.chardet@hva.nl>
2020-03-23 12:45:32 +0100

    removed legacy code

--- index.js
+++ index.js
@@ -57,44 +57,6 @@ app.get('/search', function(req, res) {
 });
 
 
-// Create a route for our overview page
-app.get('/posts', function(req, res) {
-	request('https://jsonplaceholder.typicode.com/posts', {json: true}, function (err, requestRes, body){
-		if (err) {
-			// We got an error
-			res.send(err);
-		} else {
-			// Render the page using the 'posts' view and our body data
-			res.render('posts', {
-				title: 'Posts', // We use this for the page title, see views/partials/head.ejs
-				postData: body
-			});
-		}
-	});
-});
-
-// Create a route for our detail page
-app.get('/post/:id', function(req, res) {
-	request(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`, {json: true}, function (err, requestRes, body){
-		if (err) {
-			// We got an error
-			res.send(err);
-		} else {
-			// Render the page using the 'post' view and our body data
-			res.render('post', {
-				title: `Post ${req.params.id}`, 
-				postData: body
-			});
-		}
-	});
-});
-
-// Make sure to catch /post to /posts
-app.get('/post', function(req, res) {
-	// Redirect the client using res.redirect (this will create a new request)
-	res.redirect('/posts');
-});
-
 // Actually set up the server
 app.listen(port, function() {
 	console.log(`Application started on port: ${port}`);
